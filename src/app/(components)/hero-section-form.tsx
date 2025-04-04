269047da98eb25408bc45b3b99d1472cf4928d56
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2025-04-04 02:16:14 -0300

    fix: remove garbage

--- src/app/(components)/hero-section-form.tsx
+++ src/app/(components)/hero-section-form.tsx
@@ -195,19 +195,6 @@ export function HeroSectionForm() {
   );
 }
 
-<Dialog>
-  <DialogTrigger>Open</DialogTrigger>
-  <DialogContent>
-    <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
-      </DialogDescription>
-    </DialogHeader>
-  </DialogContent>
-</Dialog>;
-
 type SuccessModalProps = {
   isOpen: boolean;
   onClose: () => void;
