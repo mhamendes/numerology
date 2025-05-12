ad94edbb2ce4501bd1a33a8daec2f7094ec62f7f
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2025-05-11 23:34:39 -0300

    feat: implement dev and prod tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,8 @@
 import { date, integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const salesTable = pgTable('sales', {
+console.log('vercelenv', process.env.VERCEL_ENV);
+
+export const prodSalesTable = pgTable('prod_sales', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
@@ -12,3 +14,19 @@ export const salesTable = pgTable('sales', {
   trackerCode: text('tracker_code').notNull().unique(),
   productId: text('product_id').notNull(),
 });
+
+export const devSalesTable = pgTable('dev_sales', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+  email: text('email').notNull(),
+  fullName: text('full_name').notNull(),
+  birthDay: date('birthday').notNull(),
+  status: text('status').notNull(),
+  locale: text('locale').notNull(),
+  trackerCode: text('tracker_code').notNull().unique(),
+  productId: text('product_id').notNull(),
+});
+
+export const salesTable =
+  process.env.VERCEL_ENV === 'production' ? prodSalesTable : devSalesTable;
