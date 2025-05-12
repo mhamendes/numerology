d18c1690b85281568db1d29cc1a1ab091cfae75f
Matheus Mendes <matheushenrique.araujomendes@coinbase.com>
2025-05-11 23:59:22 -0300

    feat: table columns as single variables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,6 @@
 import { date, integer, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
-export const prodSalesTable = pgTable('prod_sales', {
+const salesBaseColumns = {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
@@ -9,21 +9,16 @@ export const prodSalesTable = pgTable('prod_sales', {
   birthDay: date('birthday').notNull(),
   status: text('status').notNull(),
   locale: text('locale').notNull(),
-  trackerCode: text('tracker_code').notNull().unique(),
   productId: text('product_id').notNull(),
-});
+};
 
-export const devSalesTable = pgTable('dev_sales', {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at').notNull().defaultNow(),
-  email: text('email').notNull(),
-  fullName: text('full_name').notNull(),
-  birthDay: date('birthday').notNull(),
-  status: text('status').notNull(),
-  locale: text('locale').notNull(),
+export const prodSalesTable = pgTable('prod_sales', {
+  ...salesBaseColumns,
   trackerCode: text('tracker_code').notNull().unique(),
-  productId: text('product_id').notNull(),
+});
+export const devSalesTable = pgTable('dev_sales', {
+  ...salesBaseColumns,
+  trackerCode: text('tracker_code').notNull().unique(),
 });
 
 export const salesTable =
