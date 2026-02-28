2ae6519adf5ccf527580f656b69a7294d6c95c79
Matheus HMendes <matheushenrique.araujomendes@coinbase.com>
2026-02-27 23:54:09 -0300

    chore: upgrade drizzle to v1 beta

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 
 import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
 
 const connectionString = process.env.POSTGRES_URL;
 
@@ -9,5 +8,4 @@ if (!connectionString) {
   throw new Error('POSTGRES_URL is not set');
 }
 
-export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+export const db = drizzle(connectionString);
